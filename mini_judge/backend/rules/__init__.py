f72a4c7688bd8df80fbc84a1eb8dcea6575dcdfb
midi45 <155633578+midi45@users.noreply.github.com>
2025-12-15 11:58:55 +0800

    Add conditional assignment mistake check to rules

--- mini_judge/backend/rules/__init__.py
+++ mini_judge/backend/rules/__init__.py
@@ -4,11 +4,15 @@ from .brackets import check_unbalanced_brackets
 from .confusable import check_confusable_characters
 from .full_width import check_full_width_symbols
 from .quotes import check_unclosed_quotes
+from .assignment_vs_comparison import check_conditional_assignment_mistake
+
 
 from . import full_width
 from . import brackets
 from . import quotes
 from . import confusable
+from . import assignment_vs_comparison
+
 
 RuleResult = Optional[Dict[str, str]]
 
@@ -18,6 +22,7 @@ def apply_rules(code: str) -> Iterable[RuleResult]:
     yield check_unbalanced_brackets(code)
     yield check_unclosed_quotes(code)
     yield check_confusable_characters(code)
+    yield check_conditional_assignment_mistake(code)
 
 # Rule registry for frontend / metadata usage
 RULES = {
@@ -25,4 +30,6 @@ RULES = {
     "brackets": brackets,
     "quotes": quotes,
     "confusable": confusable,
+    "assignment_vs_comparison": assignment_vs_comparison,
+
 }
